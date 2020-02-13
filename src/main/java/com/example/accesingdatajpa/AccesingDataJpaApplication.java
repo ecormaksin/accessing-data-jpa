122b915ad79959a6b851d6109c68e7cbc9309954
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-14 01:30:23 +0900

    first commit

---
+++ src/main/java/com/example/accesingdatajpa/AccesingDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.accesingdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesingDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesingDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accesingdatajpa/AccesingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accesingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
